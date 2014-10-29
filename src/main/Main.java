fb4707d351b876abdb6dbacc7f786308450638b4
XanJe <xan7xan@gmail.com>
2014-10-29 21:51:38 +0200

    'commited'

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,8 @@
 package main;
 
-/**
- * Created by Jack on 29.10.2014.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hmm");
+        System.out.println("Hmm???");
     }
 }
