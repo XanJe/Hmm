6e86fe34220acb853767b62d4dfbe62e488a8afa
XanJe <xan7xan@gmail.com>
2014-11-09 03:36:19 +0200

    'message'

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,10 @@ package main;
  * Created by Jack on 29.10.2014.
  */
 public class Main {
+    /**
+     * Main method.
+     * @param args string commandline arguments
+     */
     public static void main(String[] args) {
         System.out.println("Hmm");
         System.out.println("NewBranch");
